9c20576f2db465021fc1ef9fa5118558ce25f8b3
Cecelia Freese <cecfreese@gmail.com>
2018-02-08 19:41:09 -0700

    add course work week 3

---
+++ HW3/sketch.js
@@ -0,0 +1,17 @@
+// setup() function
+function setup() {
+
+  // create canvas
+  createCanvas( 600, 400 );
+
+  // background color
+  background( 'green' );
+}
+
+
+// draw() function
+function draw(){
+
+  //p5 write "hello world" using text() function
+  text("HELLO WORLD", 300, 200 );
+}
---
+++ course work/week 3/01 hello world/sketch.js
@@ -0,0 +1,3 @@
+function setup(){
+    text("Hello Cece!", 0, 10);
+}
